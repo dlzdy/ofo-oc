21d942b0a09a9231535e14d63d83e116039a5e23
张大宇 <zhangdy@seniorkeeper.com>
2017-08-03 14:14:05 +0800

    i

---
+++ ofo-oc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ofo-oc
+//
+//  Created by zhangdy on 17/8/3.
+//  Copyright © 2017年 zhangdy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ofo-oc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ofo-oc
+//
+//  Created by zhangdy on 17/8/3.
+//  Copyright © 2017年 zhangdy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
